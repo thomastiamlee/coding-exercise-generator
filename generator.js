50e2eeb441eaa25e2da9c919cc66084d3583dba2
Thomas <thomasjamestiamlee@gmail.com>
2017-10-10 17:26:27 +0900

    Added base function for generating exercises.

--- generator.js
+++ generator.js
@@ -1,4 +1,14 @@
-/* Generate an exercise using basic operations only. */
+/* Generate an exercise using basic operations only.
+   For options, you may use the following
+	 complexity (defaults to 1): an integer value representing the number of basic operations in the generated problem */
 function generateBasicExercise(options) {
-	
+	// If no options are provided, assume it is empty
+	if (!options) {
+		options = {};
+	}
+	// Put default values for non-existent options
+	var complexity = 1;
+	if (options.complexity) {
+		complexity = parseInt(options.complexity);
+	}
 }
