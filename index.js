1e3f4ef0244dd1aa3f5338537ad3de83b5598bdf
Thomas <thomasjamestiamlee@gmail.com>
2017-11-15 14:34:30 +0900

    Added function for reading goals from the file and searching for a main goal.

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const Component = require("./src/component");
 const Restriction = require("./src/restriction");
 const Reader = require("./src/reader");
+const KBLoader = require("./src/context/kbloader.js");
+const Planner = require("./src/context/planner.js");
 
-var result = new Component.variable("number");
-var storage = [];
-Reader.loadBlocks("./test/sample/block1.blo", storage);
-var block1 = Reader.buildBlockFromInformation("average2", storage);
-block1.attachInputOperand(new Component.operand("number", 5), 0);
-block1.attachInputOperand(new Component.operand("number", 10), 1);
-block1.setVariableOutput(result);
-var returnNode = new Component.node(Component.NODE_TYPE_RETURN);
-returnNode.attachInputOperand(result, 0);
-block1.attachNode(returnNode, 0);
-var res = block1.evaluateStructure();
-console.log(block1.internalHead);
+Planner.startPlan();
