0c74cfc0e73031626e17789783c3f0cc331d0dcb
Thomas <thomasjamestiamlee@gmail.com>
2017-11-29 15:37:02 +0900

    Added basic HTML page for visualizing exercise structures using Mermaid JS.

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const Restriction = require("./src/restriction");
 const Reader = require("./src/reader");
 const Parser = require("./src/parser");
 const Planner = require("./src/planner-simple");
+const VisualizationServer = require("./src/visualization-server");
 
-Planner.planExercise(["student", "student"]);
+VisualizationServer.runServer();
