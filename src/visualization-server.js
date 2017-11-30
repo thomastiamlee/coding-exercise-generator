d711b5e5051ccd902c05e13d89efca9e2828d1ff
Thomas <thomasjamestiamlee@gmail.com>
2017-11-30 14:19:09 +0900

    Reverted back to HapiJS 16 due to compatibility issues.

--- src/visualization-server.js
+++ src/visualization-server.js
@@ -1,29 +1,59 @@
-"use strict";
+"use strict"
 
+const Path = require("path");
 const Hapi = require("hapi");
+const Vision = require("vision");
+const Nunjucks = require("nunjucks-hapi");
 
-const server = Hapi.server({
-	host: "localhost",
-	port: 8000
-});
+function start() {
+	// Create a server with a host and port
+	const server = new Hapi.Server();
+	server.connection({
+		host: "localhost",
+		port: 8000
+	});
 
-server.route({
-	method: "GET",
-	path: "/hello",
-	handler: function(request, h) {
-		return "Hello World";
-	}
-});
-
-async function start() {
-	try {
-		await server.start();
+	server.register([require("inert"), Vision], (err) => {
+		if (err) {
+			throw err;
 		}
-  catch (err) {
-		console.log(err);
-		process.exit(1);
-  }
-  console.log('Server running at:', server.info.uri);
+		
+		// Views
+		server.views({
+			engines: {
+				html: Nunjucks
+			},
+			path: "./src/views"
+		})
+		
+		// CSS, JS, and IMG resources
+		server.route({
+			method: "GET",
+			path: "/resource/{file*}",
+			handler: {
+				directory: {
+					path: "./src/resources/"
+				}
+			}
+		});
+		
+		// Main page	
+		server.route({
+			method: "GET",
+			path: "/visualize",
+			handler: function(request, reply) {
+				return reply.view("visualization.html");
+			}
+		});
+		
+		// Start the server
+		server.start((err) => {
+			if (err) {
+				throw err;
+			}
+			console.log("Server running at:", server.info.uri);
+		});
+	});
 }
 
 module.exports = {start};
