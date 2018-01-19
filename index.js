f6cc3ca61166880d08bcfdb2dbdc34cc8c422081
Thomas <thomasjamestiamlee@gmail.com>
2018-01-19 10:26:56 +0900

    Added volume computations.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const TestCaseGenerator = require("./src/test-case-generator");
 
 var kb = Parser.parseKnowledgeBase("./src/kb/experiment-space");
 var table = new PlannerUtility.memory();
-table.createLocalEntity([kb.getGlobalEntity("person"), kb.getGlobalEntity("child"), kb.getGlobalEntity("student"), kb.getGlobalEntity("school"), kb.getGlobalEntity("house"), kb.getGlobalEntity("paper"), kb.getGlobalEntity("cookie"), kb.getGlobalEntity("ball")]);
+table.createLocalEntity([kb.getGlobalEntity("person"), kb.getGlobalEntity("child"), kb.getGlobalEntity("student"), kb.getGlobalEntity("school"), kb.getGlobalEntity("house"), kb.getGlobalEntity("paper"), kb.getGlobalEntity("cookie"), kb.getGlobalEntity("ball"), kb.getGlobalEntity("dice")]);
 table.createLocalEntity([kb.getGlobalEntity("meters"), kb.getGlobalEntity("feet"), kb.getGlobalEntity("pounds"), kb.getGlobalEntity("kilograms"), kb.getGlobalEntity("celsius"), kb.getGlobalEntity("fahrenheit"), kb.getGlobalEntity("kelvin"), kb.getGlobalEntity("square"), kb.getGlobalEntity("rectangle"), kb.getGlobalEntity("triangle"), kb.getGlobalEntity("circle"), kb.getGlobalEntity("cube"), kb.getGlobalEntity("sphere")]);
 var plan = Planner.planExercise(kb, table);
 console.log(plan);
