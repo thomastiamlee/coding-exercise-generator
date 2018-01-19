5405129ccd82c3ed0fcb30dbc1d84400813b671c
Thomas <thomasjamestiamlee@gmail.com>
2018-01-19 10:15:00 +0900

    Fixed minor bugs.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const TestCaseGenerator = require("./src/test-case-generator");
 
 var kb = Parser.parseKnowledgeBase("./src/kb/experiment-space");
 var table = new PlannerUtility.memory();
-table.createLocalEntity([kb.getGlobalEntity("person"), kb.getGlobalEntity("child"), kb.getGlobalEntity("student"), kb.getGlobalEntity("school"), kb.getGlobalEntity("house"), kb.getGlobalEntity("paper"), kb.getGlobalEntity("cookie")]);
+table.createLocalEntity([kb.getGlobalEntity("person"), kb.getGlobalEntity("child"), kb.getGlobalEntity("student"), kb.getGlobalEntity("school"), kb.getGlobalEntity("house"), kb.getGlobalEntity("paper"), kb.getGlobalEntity("cookie"), kb.getGlobalEntity("ball")]);
 table.createLocalEntity([kb.getGlobalEntity("meters"), kb.getGlobalEntity("feet"), kb.getGlobalEntity("pounds"), kb.getGlobalEntity("kilograms"), kb.getGlobalEntity("celsius"), kb.getGlobalEntity("fahrenheit"), kb.getGlobalEntity("kelvin"), kb.getGlobalEntity("square"), kb.getGlobalEntity("rectangle"), kb.getGlobalEntity("triangle"), kb.getGlobalEntity("circle"), kb.getGlobalEntity("cube"), kb.getGlobalEntity("sphere")]);
 var plan = Planner.planExercise(kb, table);
 console.log(plan);
@@ -22,6 +22,8 @@ var exercise = ExerciseBuilder.buildExerciseFromActions(plan, table);
 var res = TextGenerator.convertPlanToText(plan, "./src/kb/experiment-space");
 //var testCases = TestCaseGenerator.generateTestCases(exercise, 10);
 console.log(res);
+console.log(Reader.convertToFlowchartDefinition(exercise));
+
 //console.log(res);
 //console.log(testCases);
 
