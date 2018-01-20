89bc4aa3d74bc967def4ff471ec89fad25b49657
Thomas <thomasjamestiamlee@gmail.com>
2018-01-20 19:35:59 +0900

    Added class for local memory.

--- src/planner/planner-components.js
+++ src/planner/planner-components.js
@@ -1,7 +1,20 @@
-var localEntity = function(domain, typeName) {
+var localMemory = function(domain) {
+	this.domain = domain;
+	this.localExistents = [];
+	this.localAssertions = [];
+	this.namingCounter = 1;
 	
-}
-
-var localMemory = function() {
-	
+	var localExistent = function(type) {
+		this.name = type + namingCounter;
+		this.parent = domain.getExistentByName(type);
+		this.namingCounter++;
+	}	
+	this.createLocalExistent(types) {
+		if (types instanceof Array == false) {
+			types = [types];
+		}
+		for (var i = 0; i < types.length; i++) {
+			this.localExistents.push(new localExistent(types[i]));
+		}
+	}
 }
