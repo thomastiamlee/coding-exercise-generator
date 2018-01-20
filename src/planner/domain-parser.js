28d0669cc1670c3db11c979f2d57fcc9d828b044
Thomas <thomasjamestiamlee@gmail.com>
2018-01-20 19:00:57 +0900

    Added function for parsing the actions.

--- src/planner/domain-parser.js
+++ src/planner/domain-parser.js
@@ -18,12 +18,25 @@ function parseAssertions() {
 	var parser = Peg.generate(grammar, {trace: false});
 	var result = parser.parse(existents);
 	console.log(result);
+	return result;
 }
 
+function parseActions() {
+	var grammar = File.readFileSync(grammarPath + "/actions-grammar.txt", "utf-8");
+	var parser = Peg.generate(grammar, {trace: false});
+	var result = [];
+	File.readdirSync(domainPath + "/actions").forEach(file => {
+		var actions = File.readFileSync(domainPath + "/actions/" + file.toString(), "utf-8");
+		var temp = parser.parse(actions);
+		result = result.concat(temp);
+	});
+	console.log(result);
+}
 
 function parseDomain() {
 	var existents = parseExistents();
 	var assertions = parseAssertions();
+	var actions = parseActions();
 }
 
 module.exports = {parseDomain};
