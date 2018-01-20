8bc770cda3bddde01a3b27f99cb7380ec6ee508e
Thomas <thomasjamestiamlee@gmail.com>
2018-01-20 20:13:14 +0900

    Refactored domain class into the planner-components file.

--- src/planner/domain-parser.js
+++ src/planner/domain-parser.js
@@ -1,71 +1,9 @@
 const File = require("fs");
 const Peg = require("pegjs");
+const PlannerComponents = require("./planner-components");
 
 const domainPath = "./src/planner/domain";
 const grammarPath = "./src/planner/grammar";
-
-var domain = function(existents, assertions, actions) {
-	var existent = function(name) {
-		this.name = name;
-		this.parent = null;
-		this.isExtendedFrom = function(other) {
-			var current = this;
-			while (current != null) {
-				if (current == other) return true;
-				current = this.parent;
-			}
-		}
-	}
-	var assertion = function(predicate, parameters) {
-		this.predicate = predicate;
-		this.parameters = parameters;
-	}	
-	var action = function(name, parameters, preconditions, postconditions) {
-		this.name = name;
-		this.parameters = parameters;
-		this.preconditions = preconditions;
-		this.postconditions = postconditions;
-	}
-	this.existents = [];
-	this.assertions = [];
-	this.actions = [];
-	this.getExistentByName = function(name) {
-		for (var i = 0; i < this.existents.length; i++) {
-			if (this.existents[i].name == name) {
-				return this.existents[i];
-			}
-		}
-		console.log("warning: attempted to get a non-existent existent name " + name);
-		return null;
-	}
-	for (var i = 0; i < existents.length; i++) {
-		this.existents.push(new existent(existents[i].name));
-	}
-	for (var i = 0; i < existents.length; i++) {
-		if (existents[i].parent) {
-			this.getExistentByName(existents[i].name).parent = this.getExistentByName(existents[i].parent);
-		}
-		this.existents.push(new existent(existents[i].name));
-	}
-	for (var i = 0; i < assertions.length; i++) {
-		var predicate = assertions[i].predicate;
-		var parameters = assertions[i].parameters;
-		for (var j = 0; j < parameters.length; j++) {
-			parameters[j] = this.getExistentByName(parameters[j]);
-		}
-		this.assertions.push(new assertion(predicate, parameters));
-	}
-	for (var i = 0; i < actions.length; i++) {
-		var name = actions[i].name;
-		var parameters = actions[i].parameters;
-		for (var j = 0; j < parameters.length; j++) {
-			parameters[j].type = this.getExistentByName(parameters[j].type);
-		}
-		var preconditions = actions[i].preconditions;
-		var postconditions = actions[i].postconditions;
-		this.actions.push(new action(name, parameters, preconditions, postconditions));
-	}
-}
 
 function parseExistents() {
 	var grammar = File.readFileSync(grammarPath + "/existents-grammar.txt", "utf-8");
@@ -100,7 +38,7 @@ function parseDomain() {
 	var assertions = parseAssertions();
 	var actions = parseActions();
 	
-	return new domain(existents, assertions, actions);
+	return new PlannerComponents.domain(existents, assertions, actions);
 }
 
 module.exports = {parseDomain};
--- src/planner/planner-components.js
+++ src/planner/planner-components.js
@@ -1,27 +1,65 @@
-var localMemory = function(domain) {
-	this.domain = domain;
-	this.localExistents = [];
-	this.localAssertions = [];
-	this.namingCounter = 1;
-	
-	var localExistent = function(type) {
-		this.name = type + namingCounter;
-		this.parent = domain.getExistentByName(type);
-		this.namingCounter++;
-		this.isExtendedFrom = function(other) {
-			var current = this;
-			while (current != null) {
-				if (current == other) return true;
-				current = this.parent;
-			}
-		}
-	}	
-	this.createLocalExistent(types) {
-		if (types instanceof Array == false) {
-			types = [types];
-		}
-		for (var i = 0; i < types.length; i++) {
-			this.localExistents.push(new localExistent(types[i]));
+var existent = function(name) {
+	this.name = name;
+	this.parent = null;
+	this.isExtendedFrom = function(other) {
+		var current = this;
+		while (current != null) {
+			if (current == other) return true;
+			current = this.parent;
 		}
 	}
 }
+var assertion = function(predicate, parameters) {
+	this.predicate = predicate;
+	this.parameters = parameters;
+}	
+var action = function(name, parameters, preconditions, postconditions) {
+	this.name = name;
+	this.parameters = parameters;
+	this.preconditions = preconditions;
+	this.postconditions = postconditions;
+}
+var domain = function(existents, assertions, actions) {
+	
+	this.existents = [];
+	this.assertions = [];
+	this.actions = [];
+	this.getExistentByName = function(name) {
+		for (var i = 0; i < this.existents.length; i++) {
+			if (this.existents[i].name == name) {
+				return this.existents[i];
+			}
+		}
+		console.log("warning: attempted to get a non-existent existent name " + name);
+		return null;
+	}
+	for (var i = 0; i < existents.length; i++) {
+		this.existents.push(new existent(existents[i].name));
+	}
+	for (var i = 0; i < existents.length; i++) {
+		if (existents[i].parent) {
+			this.getExistentByName(existents[i].name).parent = this.getExistentByName(existents[i].parent);
+		}
+		this.existents.push(new existent(existents[i].name));
+	}
+	for (var i = 0; i < assertions.length; i++) {
+		var predicate = assertions[i].predicate;
+		var parameters = assertions[i].parameters;
+		for (var j = 0; j < parameters.length; j++) {
+			parameters[j] = this.getExistentByName(parameters[j]);
+		}
+		this.assertions.push(new assertion(predicate, parameters));
+	}
+	for (var i = 0; i < actions.length; i++) {
+		var name = actions[i].name;
+		var parameters = actions[i].parameters;
+		for (var j = 0; j < parameters.length; j++) {
+			parameters[j].type = this.getExistentByName(parameters[j].type);
+		}
+		var preconditions = actions[i].preconditions;
+		var postconditions = actions[i].postconditions;
+		this.actions.push(new action(name, parameters, preconditions, postconditions));
+	}
+}
+
+module.exports = {domain};
